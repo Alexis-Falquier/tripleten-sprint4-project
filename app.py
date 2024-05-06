7f5c497d5f2bb1577fffbdc41d9d2fb26343a3ea
The_Swixican <25648214+Alexis-Falquier@users.noreply.github.com>
2024-05-05 20:38:44 -0700

    enhanced EDA on the notebook and changed app code per reviewer instructions. EDA: handled all missing values, made commentary on charts and added overall conclusion, (was asked to add title and axis labels to all charts but they already had them). Stremlit App: added enhanced EDA cleanup of data to the app, added a checkbox as requested, checkbox adds functionality to normalize histogram. All reviewer comments completed.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import scipy.stats
 import streamlit as st
 import plotly.express as px
+
+#START DATA CLEANUP AND ENHANCEMENT
 
 #reading in the data
 df_vehicles = pd.read_csv("./vehicles_us.csv")
@@ -13,7 +14,82 @@ df_vehicles['make'] = df_vehicles['model'].str.split().str[0]
 cols = df_vehicles.columns.tolist() 
 cols.insert(2, cols.pop(cols.index('make')))
 df_vehicles = df_vehicles[cols]
+#remove mercedes-benz
+df_vehicles.query('make != "mercedes-benz"',inplace=True)
+#drop rows with empyt model year values
+df_vehicles.dropna(subset=['model_year'],inplace=True)
+df_vehicles['model_year'] = df_vehicles['model_year'].astype("int")
 
+#fill missing values for cylinders paint color and odometer:
+
+#function to find the mode:
+def get_mode(series):
+    mode_val = series.mode()
+    if not mode_val.empty:
+        return mode_val.iloc[0]
+    return None
+
+# cylinder averages by 'model' and 'model_year'
+cyl_avg = df_vehicles.groupby(['model', 'model_year']).agg({
+    'cylinders': get_mode,
+}).reset_index()
+# cylinder averages by model
+cyl_avg_by_model = df_vehicles.groupby(['model']).agg({
+    'cylinders': get_mode,
+}).reset_index()
+
+#aggregate the cylinder averages:
+cyl_avg = cyl_avg.merge(cyl_avg_by_model, on=['model'], how='left', suffixes=('', '_by_model'))
+#fill the missing cylinder values of the model model_year pairs with the model averages
+cyl_avg['cylinders'] = cyl_avg['cylinders'].fillna(cyl_avg['cylinders_by_model'])
+
+#aggregate the paint color averages:
+#paint color averages by model
+paint_avg = df_vehicles.groupby(['model']).agg({
+    'paint_color': get_mode,
+}).reset_index()
+#merge into cylinder average dataframe
+paint_cyl_averages = cyl_avg.merge(paint_avg, on=['model'], how='left')
+#drop extra cylinder column
+paint_cyl_averages.drop(['cylinders_by_model'], axis=1, inplace=True)
+
+#aggreage odometer averages:
+#means of odometer readings by condition and by year
+odo_by_condition = df_vehicles.groupby(['condition']).agg({
+    'odometer': 'mean',
+}).reset_index()
+
+odo_by_year = df_vehicles.groupby(['model_year']).agg({
+    'odometer': 'mean',
+}).reset_index()
+
+#aggregate all averages into the df_vehicles data frame
+df_vehicles = df_vehicles.merge(paint_cyl_averages, on=['model','model_year'], how='left', suffixes=('', '_avg'))
+df_vehicles = df_vehicles.merge(odo_by_condition, on=['condition'], how='left', suffixes=('', '_avg_by_condition'))
+df_vehicles = df_vehicles.merge(odo_by_year, on=['model_year'], how='left', suffixes=('', '_avg_by_year'))
+#make an average odometer column based on the average by year and condition
+df_vehicles['odometer_avg'] = df_vehicles[['odometer_avg_by_condition', 
+                                           'odometer_avg_by_year'
+                                          ]].mean(axis=1)
+#drop extra columns
+df_vehicles.drop(['odometer_avg_by_condition',
+                  'odometer_avg_by_year'
+                 ], axis=1, inplace=True)
+
+#replace missing values in the dataframe with the averages for cylinders odometer and paint color
+for column in ['cylinders', 'odometer', 'paint_color']:
+    df_vehicles[column] = df_vehicles[column].fillna(df_vehicles[f'{column}_avg'])
+    
+#drop average columns:
+df_vehicles.drop([col for col in df_vehicles.columns if '_avg' in col], axis=1, inplace=True)
+
+#cleanup columns types
+df_vehicles['odometer'] = df_vehicles['odometer'].round().astype(int)
+df_vehicles['cylinders'] = df_vehicles['cylinders'].astype(int)
+
+#END DATA CLEANUP AND ENHANCEMENT
+
+#STREAMLIT APP CODE
 
 st.title('A brief analysis of vehicle odometer readings')
 st.text('''Below you will find a few different graphs and charts to play with 
@@ -80,15 +156,23 @@ comparison_list = [
 comparison = st.selectbox(
     label='comparisons',
     options=comparison_list, 
-    index=comparison_list.index('condition')
+    index=comparison_list.index('none')
 )
+
+#checkbox if a user wants to normalize the histogram
+normalize = st.checkbox('normalize histogram', value=False)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
 
 if comparison == 'none':
     fig=px.histogram(
         df_vehicles,
         title='Histogram of Odometer Reading Frequency',
         x='odometer',
-        nbins=100
+        nbins=100,
+        histnorm=histnorm
     )
 else:
     fig=px.histogram(
@@ -96,9 +180,10 @@ else:
         title=f'Histogram of Odometer Reading Frequency per Vehicle {comparison}',
         x='odometer',
         color=f'{comparison}',
-        nbins=100
+        nbins=100,
+        histnorm=histnorm
     )
-fig.update_layout(xaxis_title='odometer reading',yaxis_title='frequency')
+fig.update_layout(xaxis_title='Odometer Reading',yaxis_title='Number of Vehicles')
 st.write(fig)
 
 
